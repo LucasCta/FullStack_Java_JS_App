4305b81357e1e2ce6b4a624c0004540dec179de4
LucasCta <lucas.ct.araujo@gmail.com>
2023-05-25 02:09:35 -0300

    (WIP) Fixing bugs on database access

--- WEB-INF/classes/AcessoBanco.java
+++ WEB-INF/classes/AcessoBanco.java
@@ -5,36 +5,32 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.SQLException;
+
 public class AcessoBanco {
 
-	public static Connection conectar() {
+	public static Connection conectar() throws SQLException {
 		Connection con = null;
-		try {
-			Context ctx = (Context) new InitialContext().lookup("java:comp/env");
-			con = ((DataSource) ctx.lookup("jdbc/UsersTable")).getConnection();
-			return con;
-		} catch (Exception e) {
-			e.printStackTrace(System.err);
-			return null;
-		}
+		Class.forName("com.mysql.jdbc.Driver");
+		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/captchaDatabase", "lucas", "123@Teste");
+		return con;
 	}
 
-	public static boolean desconectar(Connection con, PreparedStatement ps, ResultSet rs) {
-		try {
-			if (rs != null) {
-				rs.close();
-			}
-			if (ps != null) {
-				ps.close();
-			}
-			if (con != null) {
-				con.close();
-			}
-			return true;
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return false;
+	public static boolean desconectar(Connection con, PreparedStatement ps, ResultSet SQLExceptiSQLExceptionrs)
+			throws SQLException {
+		if (rs != null) {
+			rs.close();
 		}
+		if (ps != null) {
+			ps.close();
+		}
+		if (con != null) {
+			con.close();
+		}
+		return true;
 	}
 
 }
